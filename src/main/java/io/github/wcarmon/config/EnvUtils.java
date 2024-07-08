2b516d3ef617f319f5f5bc3cfd57a6a219088782
Wil Carmon <java@wcarmon.com>
2024-07-08 12:51:18 -0400

    cleanup

---
+++ src/main/java/io/github/wcarmon/config/EnvUtils.java
@@ -0,0 +1,66 @@
+package io.github.wcarmon.config;
+
+import java.util.Locale;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.jetbrains.annotations.Nullable;
+
+/** Utilities related to environment variables */
+public final class EnvUtils {
+
+
+    /**
+     * @return delimited string of environment variables, with sorted keys and values abbreviated
+     */
+    public static String prettyPrintEnvVars() {
+        return prettyPrintEnvVars("\n", 80);
+    }
+
+    /**
+     * @param delim delimiter between environment variables
+     * @return delimited string of environment variables, with sorted keys and values abbreviated
+     */
+    public static String prettyPrintEnvVars(String delim, int maxLength) {
+        if (maxLength < 1) {
+            throw new IllegalArgumentException("maxLength must be greater than 0");
+        }
+
+        if (delim == null) {
+            delim = "";
+        }
+
+        final Map<String, String> envs = new TreeMap<>(System.getenv());
+
+        final StringBuilder sb = new StringBuilder(2048);
+
+        for (final var entry : envs.entrySet()) {
+            if (entry.getKey().toLowerCase(Locale.ROOT).contains("pass")) {
+                continue;
+            }
+
+            sb.append(entry.getKey());
+            sb.append(" = ");
+            sb.append(abbreviate(entry.getValue(), maxLength));
+            sb.append(delim);
+        }
+
+        return sb.toString();
+    }
+
+    private static String abbreviate(@Nullable String value, int maxLength) {
+        if (maxLength < 1) {
+            throw new IllegalArgumentException("maxLength must be greater than 0");
+        }
+
+        if (value == null || value.isEmpty()) {
+            return "<null>";
+        }
+
+        if (value.length() <= maxLength) {
+            return value;
+        }
+
+        return value.substring(0, maxLength - 3) + "...";
+    }
+}
